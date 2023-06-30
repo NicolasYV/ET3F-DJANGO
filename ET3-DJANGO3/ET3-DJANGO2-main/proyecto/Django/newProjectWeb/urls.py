5a314b3c1204b3093b3d58f65396c2c3505ccb9a
gros9-car <82049777+gros9-car@users.noreply.github.com>
2023-06-29 22:37:36 -0400

    Add files via upload

---
+++ ET3-DJANGO3/ET3-DJANGO2-main/proyecto/Django/newProjectWeb/urls.py
@@ -0,0 +1,25 @@
+
+from django.contrib import admin
+from django.urls import path, include 
+from . import views
+
+urlpatterns = [
+    path('index', views.index, name='index'),
+    path('login', views.login, name='login'),
+    path('form1', views.form1, name='form1'),
+    path('plants', views.plants, name='plants'),
+    path('suscripcion', views.suscripcion, name='suscripcion'),
+    path('listProducto', views.listProducto, name='listProducto'),
+    path('crud', views.crud, name='crud'),
+    path('addProducto', views.addProducto, name='addProducto'),
+    path('delProducto/<str:pk>', views.delProducto, name='delProducto'),
+    path('retModProducto/<str:pk>', views.retModProducto, name ="retModProducto"),
+    path('updProducto', views.updProducto, name="updProducto"),
+    path('formAdd', views.formAdd, name = "formAdd"),
+    path('userAdd'),
+    path('userEdit',),
+    path('userList', ),
+    path("", views.login, name="login"),
+    path("logout", views.logout, name="logout"),
+
+]
---
+++ ET3-DJANGO3/ET3-DJANGO2-main/proyecto/Django/newProjectWeb/views.py
@@ -0,0 +1,128 @@
+from django.shortcuts import render
+from .models import Producto
+
+def index(request):
+    context={}
+    return render(request, 'pages/index.html', context)
+
+def login(request):
+    context={}
+    return render(request, 'pages/login.html', context)
+
+def form1(request):
+    context={}
+    return render(request, 'pages/form1.html', context)
+
+def plants(request):
+    context={}
+    return render(request, 'pages/plants.html', context)
+
+def listProducto(request):
+    context={}
+    return render(request, 'pages/listProducto.html', context)
+
+def suscripcion(request):
+    context = {}
+    return render(request, 'pages/suscripcion.html', context)
+
+def crud(request):
+    productos = Producto.objects.all()
+    context = {'productos':productos}
+    return render(request, 'pages/listProducto.html', context)
+
+def addProducto(request):
+    if request.method != "POST":
+        context = {}
+        return render(request, 'pages/addProducto.html', context)
+    else:
+        idProducto = request.POST["id"]
+        nombreProducto = request.POST["nombre"]
+        cantidad = request.POST["cantidad"]
+        precio = request.POST["precio"]
+        estadoProducto = request.POST["estado"]
+
+        obj = Producto.objects.create(
+            idProducto = idProducto,
+            nombreProducto = nombreProducto,
+            cantidad = cantidad,
+            precio = precio,
+            estadoProducto = estadoProducto
+        )
+        print("Agregado con exito")
+        obj.save()
+        context = {'mensaje':"Producto añadido"}
+        return render (request, 'pages/addProducto.html', context)
+
+def delProducto(request,pk):
+    context = {}
+    try:
+        producto = Producto.objects.get(idProducto=pk)
+        producto.delete()
+        mensaje = "Producto eliminado"
+        productos = Producto.objects.all()
+        context = {'productos':productos,'mensaje':mensaje}
+        return render(request, 'pages/listProducto.html', context)
+    except:
+        mensaje = "Producto no encontrado"
+        productos = Producto.objects.all()
+        context = {'productos':productos, 'mensaje':mensaje}
+        return render(request, 'pages/listProducto.html', context)
+
+def retModProducto(request,pk):
+    if pk != "":
+        producto = Producto.objects.get(idProducto=pk)
+        context = {'producto':producto}
+        if producto:
+            return render(request, 'pages/editProducto.html', context)
+        else:
+            context = {'mensaje':"producto inexistente o id inválida"}
+            return render(request, 'pages/listProducto.html', context)
+
+def updProducto (request):
+    if request.method == "POST":
+        id = request.POST["id"]
+        nombreProducto = request.POST["nombre"]
+        cantidad = request.POST["cantidad"]
+        precio = request.POST["precio"]
+        estadoProducto = request.POST["estado"]
+
+        print(f"ID FRONT:  {id}")
+        producto = Producto()
+        producto.idProducto = id
+        producto.nombreProducto = nombreProducto
+        producto.cantidad = cantidad
+        producto.precio = precio
+        producto.estadoProducto = estadoProducto
+        producto.save()
+
+        context = {'mensaje':"Datos del producto actualizados",'producto':producto}
+        return render(request, 'pages/editProducto.html', context)
+    else:
+        productos = Producto.objects.all()
+        context = {'productos':productos}
+        return render(request,'pages/listProducto.html', context)
+
+def formAdd(request):
+    if request.method is not "POST":
+        return render(request, 'pages/form1.html', context)
+    else:
+        rut = request.POST["rut"]
+        nombre = request.POST["nombre"]
+        appPaterno = request.POST["apellido paterno"]
+        appMaterno = request.POST["apellido materno"]
+        fechaNacimiento = request.POST["fecha nacimiento"]
+        correo = request.POST["correo"]
+        telefono = request.POST["telefono"]
+
+        obj = Usuario.objects.create(
+            rut = rut,
+            nombre = nombre,
+            appPaterno = appPaterno,
+            appMaterno = appMaterno,
+            fechaNacimiento = fechaNacimiento,
+            correo = correo,
+            telefono = telefono
+        )
+        obj.save()
+        context = {'mensaje':"Usuario añadido"}
+        return render (request, 'pages/form1.html', context)
